d354fcc9c8184ed1aecb5b7d868d3192046bc336
wmarquesv <walace.mv@hotmail.com>
2026-05-06 17:53:08 -0300

    docker compose e alguns ajustes no projeto pronto da rocketseat

--- docker-api/dist/server.js
+++ docker-api/dist/server.js
@@ -1,10 +1,5 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const node_http_1 = __importDefault(require("node:http"));
-const server = node_http_1.default.createServer(async (request, response) => {
+import http from "node:http";
+const server = http.createServer(async (request, response) => {
     if (request.method === "GET" && request.url === "/") {
         return response.end("Hello World!");
     }
--- docker-api/src/server.ts
+++ docker-api/src/server.ts
@@ -1,11 +1,11 @@
-import http from "node:http"
+import http from "node:http";
 
 const server = http.createServer(async (request, response) => {
   if (request.method === "GET" && request.url === "/") {
-    return response.end("Hello World!")
+    return response.end("Hello World!");
   }
 
-  return response.writeHead(404).end("Not found!")
-})
+  return response.writeHead(404).end("Not found!");
+});
 
-server.listen(3333, () => console.log("Server is running on port 3333"))
+server.listen(3333, () => console.log("Server is running on port 3333"));
